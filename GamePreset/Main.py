85c7e197762b37db411f9961a03fbfe7218175e4
Guilherme Fróes | M4gic <guilhermefroes2012@gmail.com>
2025-04-01 23:22:45 -0300

    separação do preset

--- GamePreset/Main.py
+++ GamePreset/Main.py
@@ -1,7 +1,7 @@
 from Components import *
 from threading import *
 
-game = Game("Borrachinha")
+game = Game("Game")
 
 def MultThread(func):
     def wrapper():
@@ -13,7 +13,7 @@ if __name__  == "__main__":
     def __init__():
         factory = game.factory
         BlockSpr = factory.from_color(Color4(200,100,200), Vector2(200,200).tuple())
-        GameObject(game.world,BlockSpr,Vector2(20,20).tuple(),game.world)
+        GameObject(game.world,Vector2(20,20).tuple(),BlockSpr)
 
         game.play()
 
